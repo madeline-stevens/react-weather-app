4a80ecb1d6b4a23aa1096a4ba558064730073c78
Madeline Stevens <stevensm1987@gmail.com>
2018-10-29 14:34:04 -0700

    added percent to humidity and degree sign to temps

--- client/src/components/Forecast.js
+++ client/src/components/Forecast.js
@@ -7,20 +7,46 @@ class Forecast extends Component {
         {this.props.temp_min &&
           this.props.temp_max && <h1>Five Day Forecast</h1>}
         {this.props.dt_txt && <p> day: {this.props.dt_txt}</p>}
-        {this.props.temp_min && <p> min-temp: {this.props.temp_min}</p>}
-        {this.props.temp_max && <p> max-temp: {this.props.temp_max}</p>}
+        {this.props.temp_max && (
+          <p>
+            {" "}
+            High: {this.props.temp_max}
+            &deg;F{" "}
+          </p>
+        )}
+        {this.props.temp_min && (
+          <p>
+            {" "}
+            Low: {this.props.temp_min}
+            &deg;F
+          </p>
+        )}
         {this.props.descriptionTwo && (
           <p> forcasted weather: {this.props.descriptionTwo}</p>
         )}
+        <br />
         {this.props.dt_txt && <p> day: {this.props.dt_txt}</p>}
-        {this.props.temp_min && <p> min-temp: {this.props.temp_min}</p>}
-        {this.props.temp_max && <p> max-temp: {this.props.temp_max}</p>}
+        {this.props.temp_max && (
+          <p>
+            {" "}
+            High: {this.props.temp_max}
+            &deg;F
+          </p>
+        )}
+        {this.props.temp_min && (
+          <p>
+            {" "}
+            Low: {this.props.temp_min}
+            &deg;F
+          </p>
+        )}
         {this.props.descriptionTwo && (
           <p> forcasted weather: {this.props.descriptionTwo}</p>
         )}
+        <br />
         {this.props.dt_txt && <p> day: {this.props.dt_txt}</p>}
-        {this.props.temp_min && <p> min-temp: {this.props.temp_min}</p>}
-        {this.props.temp_max && <p> max-temp: {this.props.temp_max}</p>}
+        {this.props.temp_max && <p> High: {this.props.temp_max}</p>}
+        {this.props.temp_min && <p> Low: {this.props.temp_min}</p>}
         {this.props.descriptionTwo && (
           <p> forcasted weather: {this.props.descriptionTwo}</p>
         )}
--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -7,9 +7,17 @@ class Weather extends Component {
         {this.props.city && <p>city: {this.props.city}</p>}
         {this.props.country && <p>country: {this.props.country}</p>}
         {this.props.temperature && (
-          <p>current temp: {this.props.temperature}</p>
+          <p>
+            current temp: {this.props.temperature}
+            &deg;F
+          </p>
         )}
-        {this.props.humidity && <p>humidity: {this.props.humidity}</p>}
+        {this.props.humidity && (
+          <p>
+            humidity: {this.props.humidity}
+            &#37;
+          </p>
+        )}
         {this.props.description && <p>description: {this.props.description}</p>}
       </div>
     );
